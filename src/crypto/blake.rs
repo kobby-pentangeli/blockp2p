5774c86c429341ca405ba492fe600c313db0f4e7
Kobby Pentangeli <kobbypentangeli@gmail.com>
2023-01-03 03:34:47 +0000

    Minor refactor

--- src/crypto/blake.rs
+++ src/crypto/blake.rs
@@ -1,7 +1,6 @@
-use blake2b_simd::Params;
 use serde::{Deserialize, Serialize};
 
-const HASH_LEN: usize = 32;
+const LONG_HASH_LEN: usize = 32;
 const SHORT_HASH_LEN: usize = 20;
 
 /// Representation of a Blake hash
@@ -9,6 +8,14 @@ const SHORT_HASH_LEN: usize = 20;
 pub enum Blake {}
 
 impl Blake {
+    /// Creates a (long) Blake hash from bytes
+    pub fn long(src: &[u8]) -> [u8; LONG_HASH_LEN] {
+        let blake = Self::create_hash(src, LONG_HASH_LEN);
+        let mut hash: [u8; LONG_HASH_LEN] = [0; LONG_HASH_LEN];
+        hash.copy_from_slice(&blake[0..LONG_HASH_LEN]);
+        hash
+    }
+
     /// Creates a (short) Blake hash from bytes
     pub fn short(src: &[u8]) -> [u8; SHORT_HASH_LEN] {
         let blake = Self::create_hash(src, SHORT_HASH_LEN);
@@ -17,17 +24,9 @@ impl Blake {
         hash
     }
 
-    /// Creates a (long) Blake hash from bytes
-    pub fn long(src: &[u8]) -> [u8; HASH_LEN] {
-        let blake = Self::create_hash(src, HASH_LEN);
-        let mut hash: [u8; HASH_LEN] = [0; HASH_LEN];
-        hash.copy_from_slice(&blake[0..HASH_LEN]);
-        hash
-    }
-
     /// Create a Blake hash from bytes and a length param
     fn create_hash(src: &[u8], len: usize) -> Vec<u8> {
-        Params::new()
+        blake2b_simd::Params::new()
             .hash_length(len)
             .to_state()
             .update(src)
