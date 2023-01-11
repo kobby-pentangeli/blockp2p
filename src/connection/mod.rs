328ad0b23b53b828c93a8dc4cf4c839501c0781f
Kobby Pentangeli <kobbypentangeli@gmail.com>
2023-01-11 18:30:26 +0000

    `Connection`(WIP) implementation

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -1,2 +1,37 @@
+use crate::crypto::hash::Hash;
+use routing::RoutingTable;
+use std::{collections::HashMap, net::SocketAddr};
+
 /// Implements a routing table.
 pub mod routing;
+
+/// Manages the connection of a node
+#[allow(dead_code)]
+pub struct Connection {
+    entries: ConnectionMap,
+    active_connections: HashMap<Hash, SocketAddr>,
+    routing_table: RoutingTable,
+}
+
+/// Map of all connections
+pub type ConnectionMap = HashMap<SocketAddr, (Option<Hash>, ConnectionState)>;
+
+/// Connection state
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub enum ConnectionState {
+    /// Initialized outgoing connection
+    Connecting,
+    /// Incoming connection from a peer
+    Incoming,
+    /// Connection established
+    Connected,
+}
+
+/// Connection information for a node
+#[derive(Debug, Clone, Copy)]
+pub struct ConnectionInfo {
+    /// Hash of the node
+    pub hash: Hash,
+    /// Address of the node
+    pub socket_addr: SocketAddr,
+}
